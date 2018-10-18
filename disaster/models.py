d8fd576bebab2e10589266759881946529cc1365
Vaibhav <vrongmeal@gmail.com>
2018-10-18 15:14:22 +0530

    add disaster model, search disaster

--- disaster/models.py
+++ disaster/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Disaster(models.Model):
+    """Disaster model
+    """
+
+    # Keep the name lower-case always
+    name = models.CharField(
+        verbose_name='Name (Strictly Lowercase)', max_length=128)
+    # How to know if event is this disaster
+    symptoms = models.TextField(verbose_name='Symptoms')
+    # Prevent any harm
+    prevention = models.TextField(verbose_name='Prevention')
+
+    def __str__(self):
+        return self.name
--- disaster/views.py
+++ disaster/views.py
@@ -1,10 +1,21 @@
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render, get_object_or_404, get_list_or_404, Http404
 from django.views import View
+from .models import Disaster
 
 
 class SearchView(View):
     """Search as view
     """
+    template_name = 'disaster/search.html'
 
     def get(self, request):
-        return redirect('/')
+        q = request.GET['q'].lower()
+        disaster = None
+        err = None
+
+        try:
+            disaster = get_object_or_404(Disaster, name=q)
+        except Http404:
+            err = Disaster.objects.all()
+
+        return render(request, self.template_name, {'disaster': disaster, 'err': err})
