21b4def4f32bbc3b53828b12aacd727cd8700667
Supratik Das <rick.das08@gmail.com>
2018-10-25 18:46:02 +0530

    dashboard files added

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class KhaneKiCheez(models.Model):
+
+	name = models.CharField(verbose_name='Kya kha raha hai?', max_length=100, null=True, blank=True)
+	price = models.IntegerField(verbose_name='Kitne ka hai?')
+
+	def __str__(self):
+		return '{s.name}: {s.price}'.format(s=self)
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import View
+from scripts.alerts import predictEvent, predictCalamity
 
 
 class IndexView(View):
@@ -9,4 +10,16 @@ class IndexView(View):
     template_name = 'dashboard/index.html'
 
     def get(self, request):
-        return render(request, self.template_name)
+    	# Get terrorists alerts
+        terrorAlerts = predictEvent('terrorist')
+
+        # Get earthquake alerts
+        earthquakes = predictCalamity('earthquake')
+
+        # Get hurricane alerts
+        hurricanes = predictCalamity('hurricanes')
+
+        # Get tsunami alerts
+        tsunami = predictCalamity('tsunami')
+
+        return render(request, self.template_name, {'terror': terrorAlerts, 'earthquakes': earthquakes, 'hurricanes': hurricanes, 'tsunami': tsunami})
