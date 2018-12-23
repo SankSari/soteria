b0fcbb364796ced050ad488808a98a661140e44d
Vaibhav <33068691+vrongmeal@users.noreply.github.com>
2018-12-24 02:05:45 +0530

    Update models.py

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,10 +1,1 @@
 from django.db import models
-
-
-class KhaneKiCheez(models.Model):
-
-	name = models.CharField(verbose_name='Kya kha raha hai?', max_length=100, null=True, blank=True)
-	price = models.IntegerField(verbose_name='Kitne ka hai?')
-
-	def __str__(self):
-		return '{s.name}: {s.price}'.format(s=self)
